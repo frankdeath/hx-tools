d70176b401da9202efa17486d00ab5ca26ab3aa4
Kevin Peterson <frankdeath@gmail.com>
2020-01-25 22:52:54 -0600

    Allow a different number of bytes to be read when getting an int

--- util.py
+++ util.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 # Helper functions that don't need to be methods of a class
-	
+
 def getBytes(data, offset, size):
 	return data[offset:offset+size]
 
-def getInt(data, offset):
+def getInt(data, offset, size=4):
 	# Return a 32-bit int
-	return int.from_bytes(data[offset:offset+4], byteorder='little', signed=False)
+	return int.from_bytes(data[offset:offset+size], byteorder='little', signed=False)
