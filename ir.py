c0cd4b640b49dcc12feeeea3dc20339bf15dcbf8
Kevin Peterson <frankdeath@gmail.com>
2020-01-26 20:39:27 -0600

    Cleaned up imports

--- ir.py
+++ ir.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import util
-import struct
 
 class ImpulseResponse:
 	'''
--- section.py
+++ section.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import util
+import ir
+
 import zlib
 import json
 import pprint
-import util
-import ir
 
 class Section:
 	'''
